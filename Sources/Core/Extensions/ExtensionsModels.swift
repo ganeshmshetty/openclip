71f97a4c6404b3f6d6df33c297f3901c8fa33ec9
Ganesh M <ganeshm24122006@gmail.com>
2026-08-01 23:40:40 +0530

    feat(extensions): introduce ExtensionsModels and ExtensionsAPIClient in Core

---
+++ Sources/Core/Extensions/ExtensionsModels.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+public struct ExtensionItem: Sendable, Codable, Identifiable {
+    public let id: String
+    public let name: String
+    public let description: String
+    public let author: String
+    public let icon: String
+    public let category: String
+    public let downloadCount: Int
+    public let downloadURL: String
+    
+    public init(
+        id: String,
+        name: String,
+        description: String,
+        author: String,
+        icon: String,
+        category: String,
+        downloadCount: Int,
+        downloadURL: String
+    ) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.author = author
+        self.icon = icon
+        self.category = category
+        self.downloadCount = downloadCount
+        self.downloadURL = downloadURL
+    }
+}
+
+public struct ExtensionsPageResponse: Sendable, Codable {
+    public let extensions: [ExtensionItem]
+    public let page: Int
+    public let totalPages: Int
+    public let totalCount: Int
+    
+    public init(extensions: [ExtensionItem], page: Int, totalPages: Int, totalCount: Int) {
+        self.extensions = extensions
+        self.page = page
+        self.totalPages = totalPages
+        self.totalCount = totalCount
+    }
+}
