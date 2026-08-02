ea8947063125cf6c84d399e6f493fecde18b4da9
Ganesh M <ganeshm24122006@gmail.com>
2026-08-02 11:13:22 +0530

    feat(core): extract ExtensionManifest and normalized ExtensionActionKind into Manifest package

---
+++ Sources/Core/Extensions/Manifest/ExtensionManifest.swift
@@ -0,0 +1,102 @@
+import Foundation
+
+public struct ExtensionActionMetadata: Codable, Sendable, Equatable {
+    public let id: String?
+    public let title: String?
+    public let icon: String?
+    public let script: String?
+    public let url: String?
+    public let regex: String?
+    public let type: String?
+    public let scriptCode: String?
+
+    public var kind: ExtensionActionKind {
+        ExtensionActionKind(rawType: type ?? "url")
+    }
+
+    public init(
+        id: String? = nil,
+        title: String? = nil,
+        icon: String? = nil,
+        script: String? = nil,
+        url: String? = nil,
+        regex: String? = nil,
+        type: String? = nil,
+        scriptCode: String? = nil
+    ) {
+        self.id = id
+        self.title = title
+        self.icon = icon
+        self.script = script
+        self.url = url
+        self.regex = regex
+        self.type = type
+        self.scriptCode = scriptCode
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(String.self, forKey: .id)
+            ?? container.decodeIfPresent(String.self, forKey: .identifier)
+        self.title = try container.decodeIfPresent(String.self, forKey: .title)
+            ?? container.decodeIfPresent(String.self, forKey: .legacyTitle)
+        self.icon = try container.decodeIfPresent(String.self, forKey: .icon)
+            ?? container.decodeIfPresent(String.self, forKey: .legacyIcon)
+        self.script = try container.decodeIfPresent(String.self, forKey: .script)
+            ?? container.decodeIfPresent(String.self, forKey: .legacyScript)
+        self.url = try container.decodeIfPresent(String.self, forKey: .url)
+            ?? container.decodeIfPresent(String.self, forKey: .legacyURL)
+        self.regex = try container.decodeIfPresent(String.self, forKey: .regex)
+            ?? container.decodeIfPresent(String.self, forKey: .legacyRegex)
+        self.type = try container.decodeIfPresent(String.self, forKey: .type)
+        self.scriptCode = try container.decodeIfPresent(String.self, forKey: .scriptCode)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(id, forKey: .id)
+        try container.encodeIfPresent(title, forKey: .title)
+        try container.encodeIfPresent(icon, forKey: .icon)
+        try container.encodeIfPresent(script, forKey: .script)
+        try container.encodeIfPresent(url, forKey: .url)
+        try container.encodeIfPresent(regex, forKey: .regex)
+        try container.encodeIfPresent(type, forKey: .type)
+        try container.encodeIfPresent(scriptCode, forKey: .scriptCode)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case identifier = "identifier"
+        case title = "title"
+        case legacyTitle = "Title"
+        case icon = "icon"
+        case legacyIcon = "Icon"
+        case script = "script"
+        case legacyScript = "Script"
+        case url = "url"
+        case legacyURL = "URL"
+        case regex = "regex"
+        case legacyRegex = "Regular Expression"
+        case type = "type"
+        case scriptCode = "scriptCode"
+    }
+}
+
+public struct ExtensionManifest: Codable, Sendable, Equatable {
+    public let identifier: String
+    public let name: String
+    public let version: String?
+    public let actions: [ExtensionActionMetadata]
+
+    public init(
+        identifier: String,
+        name: String,
+        version: String? = "1.0.0",
+        actions: [ExtensionActionMetadata]
+    ) {
+        self.identifier = identifier
+        self.name = name
+        self.version = version
+        self.actions = actions
+    }
+}
--- Tests/OpenClipTests/ExtensionManifestTests.swift
+++ Tests/OpenClipTests/ExtensionManifestTests.swift
@@ -1,132 +1,36 @@
 import XCTest
-@testable import OpenClip
 @testable import Core
+@testable import OpenClip
 
 final class ExtensionManifestTests: XCTestCase {
-    func testDecodeManifestWithIdKey() throws {
+    func testExtensionActionKindNormalization() {
+        XCTAssertEqual(ExtensionActionKind(rawType: "url"), .url)
+        XCTAssertEqual(ExtensionActionKind(rawType: "js"), .js)
+        XCTAssertEqual(ExtensionActionKind(rawType: "applescript"), .applescript)
+        XCTAssertEqual(ExtensionActionKind(rawType: "shellInline"), .shellInline)
+        XCTAssertEqual(ExtensionActionKind(rawType: "scriptFile"), .scriptFile)
+    }
+
+    func testManifestDecoding() throws {
         let json = """
         {
-          "id": "com.example.idtest",
-          "name": "Test Extension with id",
-          "options": [
-            {
-              "identifier": "api_key",
-              "label": "API Key",
-              "type": "string",
-              "default": "default_val"
-            }
-          ],
+          "identifier": "com.example.translator",
+          "name": "Translator",
+          "version": "1.0.0",
           "actions": [
             {
-              "title": "Run Script",
-              "script": "main.js",
-              "icon": "symbol:play"
+              "id": "action.translate",
+              "title": "Translate",
+              "type": "url",
+              "url": "https://translate.google.com/?text={query}"
             }
           ]
         }
         """.data(using: .utf8)!
 
-        let metadata = try JSONDecoder().decode(ExtensionMetadata.self, from: json)
-        XCTAssertEqual(metadata.identifier, "com.example.idtest")
-        XCTAssertEqual(metadata.name, "Test Extension with id")
-        XCTAssertEqual(metadata.options?.count, 1)
-        XCTAssertEqual(metadata.options?.first?.identifier, "api_key")
-    }
-
-    func testDecodeManifestWithIdentifierKey() throws {
-        let json = """
-        {
-          "identifier": "com.example.identifiertest",
-          "name": "Test Extension with identifier",
-          "actions": [
-            {
-              "title": "Search Action",
-              "url": "https://example.com/search?q={query}"
-            }
-          ]
-        }
-        """.data(using: .utf8)!
-
-        let metadata = try JSONDecoder().decode(ExtensionMetadata.self, from: json)
-        XCTAssertEqual(metadata.identifier, "com.example.identifiertest")
-        XCTAssertEqual(metadata.name, "Test Extension with identifier")
-        XCTAssertEqual(metadata.actions.count, 1)
-        XCTAssertEqual(metadata.actions.first?.url, "https://example.com/search?q={query}")
-    }
-
-    func testDecodeLegacyPascalCaseManifest() throws {
-        let json = """
-        {
-          "Identifier": "com.example.legacy",
-          "Name": "Legacy Extension",
-          "Actions": [
-            {
-              "Title": "Legacy Script Action",
-              "Script": "run.sh",
-              "Icon": "symbol:star"
-            },
-            {
-              "Title": "Legacy URL Action",
-              "URL": "https://legacy.example.com",
-              "Regular Expression": "https?://.*"
-            }
-          ]
-        }
-        """.data(using: .utf8)!
-
-        let metadata = try JSONDecoder().decode(ExtensionMetadata.self, from: json)
-        XCTAssertEqual(metadata.identifier, "com.example.legacy")
-        XCTAssertEqual(metadata.name, "Legacy Extension")
-        XCTAssertEqual(metadata.actions.count, 2)
-
-        let action1 = metadata.actions[0]
-        XCTAssertEqual(action1.title, "Legacy Script Action")
-        XCTAssertEqual(action1.script, "run.sh")
-        XCTAssertEqual(action1.icon, "symbol:star")
-
-        let action2 = metadata.actions[1]
-        XCTAssertEqual(action2.title, "Legacy URL Action")
-        XCTAssertEqual(action2.url, "https://legacy.example.com")
-        XCTAssertEqual(action2.regex, "https?://.*")
-    }
-
-    func testDecodeOptionsWithCamelCaseAndPascalCaseFallback() throws {
-        let json = """
-        {
-          "id": "com.example.options",
-          "name": "Options Test",
-          "Actions": [],
-          "Options": [
-            {
-              "Identifier": "opt_pascal",
-              "Label": "Pascal Option",
-              "Type": "string",
-              "Default": "val_pascal"
-            },
-            {
-              "id": "opt_camel",
-              "label": "Camel Option",
-              "type": "boolean",
-              "default": "true"
-            }
-          ]
-        }
-        """.data(using: .utf8)!
-
-        let metadata = try JSONDecoder().decode(ExtensionMetadata.self, from: json)
-        XCTAssertEqual(metadata.identifier, "com.example.options")
-        XCTAssertEqual(metadata.options?.count, 2)
-
-        let opt1 = metadata.options?[0]
-        XCTAssertEqual(opt1?.identifier, "opt_pascal")
-        XCTAssertEqual(opt1?.label, "Pascal Option")
-        XCTAssertEqual(opt1?.type, "string")
-        XCTAssertEqual(opt1?.defaultValue, "val_pascal")
-
-        let opt2 = metadata.options?[1]
-        XCTAssertEqual(opt2?.identifier, "opt_camel")
-        XCTAssertEqual(opt2?.label, "Camel Option")
-        XCTAssertEqual(opt2?.type, "boolean")
-        XCTAssertEqual(opt2?.defaultValue, "true")
+        let manifest = try JSONDecoder().decode(ExtensionManifest.self, from: json)
+        XCTAssertEqual(manifest.identifier, "com.example.translator")
+        XCTAssertEqual(manifest.actions.count, 1)
+        XCTAssertEqual(manifest.actions[0].kind, .url)
     }
 }
